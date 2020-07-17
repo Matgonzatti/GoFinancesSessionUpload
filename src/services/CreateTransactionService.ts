f38cc7b59f77f08f33315a902cb797ccf3f5014d
Matheus Gonzatti <matheus.gonzatti@gmail.com>
2020-07-17 08:59:47 -0300

    Chore: Creating transactions features.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,57 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import TransactionRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoryRepository = getRepository(Category);
+
+    let categoryExists = await categoryRepository.findOne({
+      where: [
+        {
+          title: category,
+        },
+      ],
+    });
+
+    if (!categoryExists) {
+      categoryExists = categoryRepository.create({ title: category });
+
+      await categoryRepository.save(categoryExists);
+    }
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total - value < 0) {
+      throw new AppError('You dont have money enough to this outcome');
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryExists.id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
